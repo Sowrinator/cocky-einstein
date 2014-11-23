51b1cd1c4ffa9e8b471e5d0e9eecc41813b77503
Sowrinator <david.sowry@gmail.com>
2014-11-24 09:05:36 +1300

    Added all current project files to the repository

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ cocky-einstein
 ==============
 
 Simply Docker
+
+Basic Docker Application that uses a node docker container and a postgres docker container to connect to a simple application.
+Uses the data container pattern.
---
+++ node/src/script.js
@@ -0,0 +1,26 @@
+var pg = require('pg');
+
+var tcp_addr = process.env.POSTGRES_PORT_5432_TCP_ADDR;
+var tcp_port = process.env.POSTGRES_PORT_5432_TCP_PORT;
+
+console.log("tcp_addr: " + tcp_addr);
+console.log("tcp_port: " + tcp_port);
+
+var client = new pg.Client({
+    user: 'docker',
+    password: 'docker',
+    database: 'docker',
+    host: tcp_addr,
+    port: tcp_port
+});
+
+pg.connect(client, function(err, client) {
+    var select_query = client.query({
+        text: "SELECT * FROM einstein",
+        name: "select"
+    });
+
+    select_query.on('row', function(row) {
+        console.log(row);
+    });
+});
