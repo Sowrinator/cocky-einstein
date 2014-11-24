a5602704a93b10b6483e61c074fe06cb289c27bc
Sowrinator <david.sowry@gmail.com>
2014-11-24 15:37:17 +1300

    Added password to postgres, and docker user

--- postgres/src/postgres/setup-my-schema.sh
+++ postgres/src/postgres/setup-my-schema.sh
@@ -2,6 +2,8 @@
 
 gosu postgres postgres --single <<- EOSQL
    CREATE DATABASE docker;
+   ALTER USER postgres WITH PASSWORD 'postgres';
    CREATE USER docker;
+   ALTER USER docker WITH PASSWORD 'docker';
    GRANT ALL PRIVILEGES ON DATABASE docker to docker;
 EOSQL
