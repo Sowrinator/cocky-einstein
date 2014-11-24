c4bb32eba1daee694f05b7afa39995c0bed43796
Sowrinator <david.sowry@gmail.com>
2014-11-24 16:30:20 +1300

    Created table einstein which is now inserted by script with values

--- postgres/src/postgres/database-init.sh
+++ postgres/src/postgres/database-init.sh
@@ -2,8 +2,11 @@
 
 gosu postgres postgres --single <<- EOSQL
    CREATE DATABASE docker;
+EOSQL
+gosu postgres postgres --single docker <<- EOSQL
+   CREATE TABLE einstein ( col1 varchar(255) );
+   INSERT INTO einstein VALUES ( 'Hi, Its Postgres' );
    ALTER USER postgres WITH PASSWORD 'postgres';
-   CREATE USER docker;
-   ALTER USER docker WITH PASSWORD 'docker';
-   GRANT ALL PRIVILEGES ON DATABASE docker to docker;
+   CREATE USER docker WITH PASSWORD 'docker';
+   ALTER ROLE docker SUPERUSER;
 EOSQL
